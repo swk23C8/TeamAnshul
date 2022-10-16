904ca7c486406b8b44d4ea8630d3a68d0c46a5d5
Michael202525 <michaeljones202525@gmail.com>
2022-10-16 17:33:00 +1000

    Replaced files with tute files. Added DB browser.

--- website/forms.py
+++ website/forms.py
@@ -1,23 +1,46 @@
-
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
 
+ALLOWED_FILE = {'PNG','JPG','png','jpg'}
 
-#creates the login information
+#Create new destination
+class DestinationForm(FlaskForm):
+  name = StringField('Name:', validators=[InputRequired()])
+  description = TextAreaField('Description:', 
+            validators=[InputRequired()])
+  image = FileField('Event Image:', validators=[
+    FileRequired(message='Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
+  currency = StringField('Currency', validators=[InputRequired()])
+  #Have to change fields and add vars in class in models.py
+  #ticket_num = StringField('Number of Tickets:', validators=[InputRequired()])
+  #ticket_Price = StringField('Price of Each Ticket:', validators=[InputRequired()])
+  #event_date = StringField('When is this Event Happening?', validators=[InputRequired()])
+  #starting_date = StringField('When are tickets opening?', validators=[InputRequired()])
+  #closing_date = StringField('When are tickets closing?', validators=[InputRequired()])
+  submit = SubmitField("Create")
+    
+#User login
 class LoginForm(FlaskForm):
     user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
     password=PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
+#User register
 class RegisterForm(FlaskForm):
     user_name=StringField("User Name", validators=[InputRequired()])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    
     #linking two fields - password should be equal to data entered in confirm
     password=PasswordField("Password", validators=[InputRequired(),
                   EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
-
     #submit button
     submit = SubmitField("Register")
+
+#User comment
+class CommentForm(FlaskForm):
+  text = TextAreaField('Comment', [InputRequired()])
+  submit = SubmitField('Create')
--- website/views.py
+++ website/views.py
@@ -1,8 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect,url_for
+from .models import Destination 
 
-bp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__)
 
-
-@bp.route('/')
+@mainbp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    destinations = Destination.query.all()    
+    return render_template('index.html', destinations=destinations)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search']:
+        print(request.args['search'])
+        dest = "%" + request.args['search'] + '%'
+        destinations = Destination.query.filter(Destination.description.like(dest)).all()
+        return render_template('index.html', destinations=destinations)
+    else:
+        return redirect(url_for('main.index'))
